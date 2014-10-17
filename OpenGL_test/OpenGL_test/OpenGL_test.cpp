a9d8d7f8d75b10db891bdcd896ea855d41c11e28
ryudai ishikawa <ryu04052014@yahoo.co.jp>
2014-10-17 14:56:36 +0900

    w't 4_2

--- OpenGL_test/OpenGL_test/OpenGL_test.cpp
+++ OpenGL_test/OpenGL_test/OpenGL_test.cpp
@@ -11,12 +11,17 @@
 
 void display()
 {
+	glClearColor(0.0, 0.0, 1.0, 0.0);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glFlush();
+
 }
 
 
 int main(int argc, char * argv[])
 {
 	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_RGBA);
 	glutCreateWindow(argv[0]);
 	glutDisplayFunc(display);
 	glutMainLoop();
